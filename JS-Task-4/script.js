7891d022e353f3666ba7f0593a3ce8d6ab0924f9
nihadaliyev <nihadali1303@gmail.com>
2025-04-07 14:09:59 +0400

    feat: JS-Task-04 completed

---
+++ JS-Task-4/script.js
@@ -0,0 +1,109 @@
+// Task 1
+
+// function logGreeting(name) {
+//     console.log(name + " is a JS Developer!");
+// }
+
+// logGreeting("Nihad");
+
+// Task 2
+
+// function logGreeting(userName) {
+//     console.log(userName + " is active now!");
+// }
+
+// logGreeting("Nihad");
+// logGreeting("Cavid");
+// logGreeting("Musa");
+
+// Task 3
+
+// function findLargestNumber(a, b, c) {
+//     let largest = a;
+
+//     if (num2 > largest) {
+//         largest = b;
+//     }
+//     if (num3 > largest) {
+//         largest = c;
+//     }
+
+//     console.log("The largest number is:", largest);
+// }
+
+// findLargestNumber(5, 10, 3);
+// findLargestNumber(7, 7, 7);
+// findLargestNumber(12, 15, 9);
+
+// Task 4
+
+// function calculateTotalAmount(rubleAccount, dollarAccount, exchangeRate) {
+//   let totalInRubles = rubleAccount + dollarAccount * exchangeRate;
+
+//   console.log(`Amount on all deposits: ${totalInRubles} rubles.`);
+// }
+
+// calculateTotalAmount(1200, 20, 75);
+
+// Task 5
+
+// function min(a, b) {
+//   let minNumber = a;
+
+//   if (minNumber > b) {
+//     minNumber = b;
+//   }
+
+//   console.log("Min number is:", minNumber);
+// }
+
+// min(5, 3);
+
+// Task 6
+
+// function concatenateStrings(a, b) {
+//     console.log(a + b);
+// }
+
+// concatenateStrings("Salam ", "Fuad!");
+
+// Task 7
+
+// function calculateRectangleArea(length, width) {
+//   console.log("The area of the rectangle is:", length * width);
+// }
+
+// calculateRectangleArea(7, 10);
+
+// Task 8
+
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     console.log(`${number} is an even number.`);
+//   } else {
+//     console.log(`${number} is an odd number.`);
+//   }
+// }
+
+// isEven(4);
+// isEven(7);
+
+// Task 9
+
+// function isPrime(number) {
+//     if (number <= 1) {
+//         return false; 
+//     }
+
+    
+//     for (let i = 2; i < number; i++) {
+//         if (number % i === 0) {
+//             return false; 
+//         }
+//     }
+
+//     return true; 
+// }
+
+// console.log(isPrime(9));  
+// console.log(isPrime(11)); 
