d5238e606b1503159e1c5ef5718cf1317260e6c7
nihadaliyev <nihadali1303@gmail.com>
2025-04-28 17:32:39 +0400

    feat: JS-Array-01-Tasks completed

---
+++ JS-Array-01/script.js
@@ -0,0 +1,124 @@
+// Task-1
+
+// let groupmates = ["Əli", "Tahir", "Aytən"];
+
+// groupmates.push("Sabir");
+
+// groupmates.shift();
+
+// console.log(groupmates);
+
+// Task-2
+
+// let names = ["Əli", "Tahir", "Aytən"];
+
+// names[1] = "Classified";
+
+// console.log(names);
+
+// Task-3
+
+// let originalArray = [1, 2, 3, 4, 5];
+
+// let copiedArray = [...originalArray];
+
+// copiedArray = copiedArray.map((num) => num * 2);
+
+// console.log(originalArray);
+// console.log(copiedArray);
+
+// Task-4
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jedi'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`Episode ${i + 1}: ${arr[i]}`);
+// }
+
+// Task-5
+
+// let userInput = prompt("Please enter five numbers separated by commas:");
+
+// let numbers = userInput.split(',').map(Number);
+
+// let smallestNumber = Math.min(...numbers);
+
+// console.log(numbers);
+// console.log(smallestNumber);
+
+// Task-6
+
+// let text = '32, 31, 34, 36, 31';
+
+// let arr = text.split(', ');
+
+// let modifiedText = arr.join('; ');
+
+// console.log(modifiedText);
+
+// Task-7
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+// Task-8
+
+// let names = ["Əli", "Tahir"];
+
+// let ages = [20, 21];
+
+// let mixed = [];
+
+// for (let i = 0; i < names.length; i++) {
+//   mixed.push(names[i] + ": " + ages[i]);
+// }
+
+// console.log(mixed);
+
+// Task-9
+
+// let numbers = [5, 8, 3, 12, 6];
+
+// let largest = Math.max(...numbers);
+// let smallest = Math.min(...numbers);
+
+// let difference = largest - smallest;
+
+// console.log(difference);
+
+// Task-10
+
+// let words = ["alma", "armud", "heyva"];
+
+// let upperCaseWords = words.map(word => word.toUpperCase());
+
+// console.log(upperCaseWords);
+
+// Task-11
+
+// let numbers = [3, -1, 5, -7, 2];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < 0) {
+//     numbers[i] = 0;
+//   }
+// }
+
+// console.log(numbers);
+
+// Task-12
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log(+sum);
